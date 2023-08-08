96188594689d35ef6b55fb4ad6e06d3178fd161b
SivakumarParthiban <pvpsiva@gmail.com>
2023-08-07 22:15:16 -0400

    Project Initial Check-in
    project created using project initializer

---
+++ SivaPetClinic/src/main/java/com/spring5/sivapetclinic/SivaPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.sivapetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SivaPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SivaPetClinicApplication.class, args);
+	}
+
+}
