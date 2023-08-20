a99c1298f7d5dbffef55998720efd794f00d1668
SivakumarParthiban <pvpsiva@gmail.com>
2023-08-19 20:32:50 -0400

    fixed POM file and hashcode and equals
    fixed POM file and hashcode and equals

--- SivaPetClinic/pet-clinic-data/src/main/java/com/spring5/sivapetclinic/model/Owner.java
+++ SivaPetClinic/pet-clinic-data/src/main/java/com/spring5/sivapetclinic/model/Owner.java
@@ -1,5 +1,6 @@
 package com.spring5.sivapetclinic.model;
 
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -14,4 +15,24 @@ public class Owner extends Person {
 	private String zipcode;
 	private String telephone;
 	private Set<Pet> pets;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Owner other = (Owner) obj;
+		return Objects.equals(telephone, other.telephone);
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(telephone);
+		return result;
+	}
+	
+	
 }
--- SivaPetClinic/pet-clinic-data/src/main/java/com/spring5/sivapetclinic/model/Pet.java
+++ SivaPetClinic/pet-clinic-data/src/main/java/com/spring5/sivapetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.spring5.sivapetclinic.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import lombok.Data;
@@ -14,5 +15,28 @@ public class Pet extends BaseEntity {
 	private LocalDate birthDate;
 
 	private String name;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pet other = (Pet) obj;
+		return Objects.equals(birthDate, other.birthDate) && Objects.equals(name, other.name)
+				&& Objects.equals(owner, other.owner) && Objects.equals(petType, other.petType);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(birthDate, name, owner, petType);
+		return result;
+	}
+	
+	
 
 }
